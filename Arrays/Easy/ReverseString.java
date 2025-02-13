128016a270bf9cc4834c00843352ab9ae031b18d
Hari <haris.22aim@kongu.edu>
2025-02-13 09:10:12 +0530

    questions added

---
+++ Arrays/Easy/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        byte [] arr = str.getBytes();
+        byte [] result=new byte[str.length];
+        for(int i=0;i<arr.length;i++){
+            res[i]=arr[arr.length-i-1]
+        }
+        System.out.println(new String (res));
+    }
+}
