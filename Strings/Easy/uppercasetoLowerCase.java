2febdbeef4d807a08a818c35442456c1ba9bee60
Hari <haris.22aim@kongu.edu>
2025-03-11 21:51:44 +0530

    strings

---
+++ Strings/Easy/uppercasetoLowerCase.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "HELLO WORLD";
+        String lowerStr = str.toLowerCase();
+        System.out.println(lowerStr);
+    }
+}
